4f031cd503ad0091491c16c84514d5527b0d0d5b
Ariel Davis <ariel.z.davis@icloud.com>
2020-01-10 16:03:16 -0500

    Use spaces for rb

--- fix-ws.rb
+++ fix-ws.rb
@@ -1,12 +1,12 @@
 class FixWs < Formula
-	desc "Fixes whitespace"
-	homepage "https://github.com/azdavis/fix-ws"
-	# TODO change the url so that it auto-detects the version?
-	version "0.1.0"
-	url "https://github.com/azdavis/fix-ws/releases/download/v0.1.0/fix-ws-mac.tar.gz"
-	sha256 "b925693a8f9067bea88bd387e6940eb273231907c6d969ce60e80bd2db5b9f90"
+  desc "Fixes whitespace"
+  homepage "https://github.com/azdavis/fix-ws"
+  # TODO change the url so that it auto-detects the version?
+  version "0.1.0"
+  url "https://github.com/azdavis/fix-ws/releases/download/v0.1.0/fix-ws-mac.tar.gz"
+  sha256 "b925693a8f9067bea88bd387e6940eb273231907c6d969ce60e80bd2db5b9f90"
 
-	def install
-		bin.install "fix-ws"
-	end
+  def install
+    bin.install "fix-ws"
+  end
 end
