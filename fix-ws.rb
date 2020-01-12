6ed9f093b9a531ee673a571d3d7c7756323fa4f5
Ariel Davis <ariel.z.davis@icloud.com>
2020-01-12 13:40:00 -0500

    Add version again

--- fix-ws.rb
+++ fix-ws.rb
@@ -1,6 +1,7 @@
 class FixWs < Formula
   desc "Fixes whitespace"
   homepage "https://github.com/azdavis/fix-ws"
+  version "0.2.0"
   url "https://github.com/azdavis/fix-ws/releases/download/v0.2.0/fix-ws-v0.2.0-macos.tar.gz"
   sha256 "c8cba3da8ada033181fe4c02a7d9c56a5222132205b3ea539fb39f1e9955fbd2"
 
