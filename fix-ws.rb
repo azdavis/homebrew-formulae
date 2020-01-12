83e01f14135c98a31ef14df59baf0e4cff5abca7
Ariel Davis <ariel.z.davis@icloud.com>
2020-01-12 13:34:43 -0500

    Update fix-ws v0.2.0

--- fix-ws.rb
+++ fix-ws.rb
@@ -1,9 +1,7 @@
 class FixWs < Formula
   desc "Fixes whitespace"
   homepage "https://github.com/azdavis/fix-ws"
-  # TODO change the url so that it auto-detects the version?
-  version "0.1.0"
-  url "https://github.com/azdavis/fix-ws/releases/download/v0.1.0/fix-ws-mac.tar.gz"
+  url "https://github.com/azdavis/fix-ws/releases/download/v0.2.0/fix-ws-v0.2.0-macos.tar.gz"
   sha256 "b925693a8f9067bea88bd387e6940eb273231907c6d969ce60e80bd2db5b9f90"
 
   def install
