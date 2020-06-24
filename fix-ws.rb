d8d74a29a3d54b21352867f5858ae10c33865ab6
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-23 17:02:35 -0700

    Add fix-ws 0.3.0

--- fix-ws.rb
+++ fix-ws.rb
@@ -1,9 +1,9 @@
 class FixWs < Formula
   desc "Fixes whitespace"
   homepage "https://github.com/azdavis/fix-ws"
-  version "0.2.0"
-  url "https://github.com/azdavis/fix-ws/releases/download/v0.2.0/fix-ws-v0.2.0-macos.tar.gz"
-  sha256 "c8cba3da8ada033181fe4c02a7d9c56a5222132205b3ea539fb39f1e9955fbd2"
+  version "0.3.0"
+  url "https://github.com/azdavis/fix-ws/releases/download/v0.3.0/fix-ws-macos.tar.gz"
+  sha256 "1c4b08ff40ed31561f78e2d4445a1544f109e253d8f8b82e1a707184608db129"
 
   def install
     bin.install "fix-ws"
